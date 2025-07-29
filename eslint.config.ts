7939b625eb4b2027054f64c9947e537080834055
Dmytro Portianka <portianka.d@gmail.com>
2025-07-28 22:29:27 -0400

    fix styling

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import stylistic from '@stylistic/eslint-plugin';
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config(
+  {
+    ignores: ['build', 'node_modules', 'public'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
@@ -15,19 +23,15 @@ export default tseslint.config(
     jsx: true,
   }),
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     languageOptions: {
       globals: {
         ...globals.browser,
       },
     },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
+    ignores: ['**/build/**', 'node_modules', 'public'],
     rules: {
       'react/react-in-jsx-scope': 'off',
     },
-  }
-);
+  },
+)
