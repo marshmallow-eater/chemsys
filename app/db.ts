5459b312baeb099f7abac5ecfc2020fd73e9bf0f
Dmytro Portianka <portianka.d@gmail.com>
2025-08-04 00:06:07 -0400

    fixed codestyle

--- app/db.ts
+++ app/db.ts
@@ -2,6 +2,6 @@ import Nano from 'nano'
 
 const env = process.env
 
-const n = Nano(`http://${env.COUCHDB_USER}:${env.COUCHDB_PASSWORD}@${env.COUCH_HOST??'localhost'}:${env.COUCHDB_PORT}`)
+const n = Nano(`http://${env.COUCHDB_USER}:${env.COUCHDB_PASSWORD}@${env.COUCH_HOST ?? 'localhost'}:${env.COUCHDB_PORT}`)
 
 export const db = n.db.use(process.env.COUCHDB_DBNAME as string)
