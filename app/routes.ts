f0a84051bd7d34151bfad6977d29bb613448b375
Dmytro Portianka <portianka.d@gmail.com>
2025-08-08 08:12:38 -0400

    reformat

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,3 @@
-import { type RouteConfig, index, route } from '@react-router/dev/routes'
+import { index, route, type RouteConfig } from '@react-router/dev/routes'
 
 export default [index('routes/home.tsx'), route('*', 'routes/notFound.tsx')] satisfies RouteConfig
