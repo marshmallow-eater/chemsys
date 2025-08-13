0ad15784293863ed653adadcba9f187880853f25
Dmytro Portianka <portianka.d@gmail.com>
2025-08-12 23:37:41 -0400

    created modal for product with retrival query

--- app/model/Products.ts
+++ app/model/Products.ts
@@ -1,35 +1,46 @@
 import { Schema } from 'mongoose'
 
 import { db } from '~/db'
+import type { LanguageCode } from '~/supportedLanguage'
 
 import { localizedStringSchema } from './localizedString'
-import type { LanguageCode } from '~/supportedLanguage'
 
 const productSchema = new Schema({
   code: { type: String, required: true },
   title: { type: localizedStringSchema, required: true },
   description: { type: localizedStringSchema, required: true },
+  applience: { type: localizedStringSchema },
   price: { type: Number }, // ALL prices considered to be in UAH
   priceSetOn: { type: Date }, // when price were set, we can hide price if it was set more than 24 hours ago
   avaliability: { type: Boolean }, // If this product avaliable to immidiatly send to client
   avaliabilitySetOn: { type: Boolean }, // when avaliablity was set
   synonims: { type: [localizedStringSchema] },
-  // TODO: add more fields
-  // country of origin
-  // packaging
-  // international name
-  // chemical formula
-  // tags, might or no as it is similar to synonims
-  // industries where it is used
-  // how it is used in this industries
+  origin: { type: localizedStringSchema },
+  packaging: { type: localizedStringSchema },
+  internationalName: { type: String },
+  chemicalFormula: { type: String },
+  industries: { type: [localizedStringSchema] },
 })
 
 const Product = db.models.Products || db.model('Products', productSchema)
 
-export const getByGroup = async (lang: LanguageCode) => {
-  return Product.find({})
+export const getByIndustrie = async (lang: LanguageCode, industryName: string) => {
+  return Product.find({
+    industries: {
+      $elemMatch: { [lang]: new RegExp(industryName, 'i') },
+    },
+  })
 }
 
-export const getByNameOrTag = async (lang: LanguageCode) => {
-  return Product.find({})
+export const getByNameOrTag = async (lang: LanguageCode, searchPhrase: string) => {
+  const searchRegex = new RegExp(`.*${searchPhrase}.*`)
+
+  return Product.find({
+    $or: [
+      { [`title.${lang}`]: searchRegex },
+      { synonims: { $elemMatch: { [lang]: searchRegex } } },
+      { internationalName: searchRegex },
+      { code: searchPhrase },
+    ],
+  })
 }
